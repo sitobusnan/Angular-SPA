16eb6986a914af18c7426559f0d13348afb9f98c
sitobusnan <sito@citytonight.es>
2020-02-18 11:47:50 +0100

    heroes component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,29 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { HomeComponent } from './components/home/home.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
+import { AboutComponent } from './components/about/about.component';
 
-const routes: Routes = [];
+
+const ROUTES: Routes = [
+    { path: 'home', component: HomeComponent },
+    { path: 'about', component: AboutComponent },
+    { path: 'heroes', component: HeroesComponent },
+    // { path: 'path2', component: Name2Component },
+    // { path: 'path3', component: Name3Component },
+    // { path: 'path4', component: Name4Component },
+    { path: '', pathMatch: 'full', redirectTo: 'home' },
+
+    // { path: 'path/:routeParam', component: MyComponent },
+    // { path: 'staticPath', component: ... },
+    // { path: '**', component: ... },
+    // { path: 'oldPath', redirectTo: '/staticPath' },
+    // { path: ..., component: ..., data: { message: 'Custom' }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(ROUTES)],
+    exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// Rutas
 import { AppRoutingModule } from './app-routing.module';
+
+// Services
+
+// Componentes
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
